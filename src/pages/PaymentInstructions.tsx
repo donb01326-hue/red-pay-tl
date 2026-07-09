4ce24cbfd9f9bafc6467f2f0d5d96465d38317a3
donb01326-hue <donb01326@gmail.com>
2026-07-09 19:03:21 +0100

    Update PaymentInstructions.tsx

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "9017554887";
-  const bankName = "SMART CASH";
-  const accountName = "KIKELOMO OLUWASANMI";
+  const accountNumber = "6543013201";
+  const bankName = "MONIEPOINT MFB";
+  const accountName = "LEMIKAN OLUWASEGUN";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
